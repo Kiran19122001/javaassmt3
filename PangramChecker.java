dd6a94d9b1e03af1175b27430e9452aad6a7f47b
Kiran19122001 <kiranban0011@gmail.com>
2023-10-12 11:44:51 +0530

    assigment3

---
+++ PangramChecker.java
@@ -0,0 +1,33 @@
+package javaass1;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class PangramChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String input = scanner.nextLine().toLowerCase();
+
+        boolean isPangram = isPangram(input);
+
+        if (isPangram) {
+            System.out.println("The input is a pangram.");
+        } else {
+            System.out.println("The input is not a pangram.");
+        }
+    }
+
+    public static boolean isPangram(String input) {
+        Set<Character> alphabetSet = new HashSet<>();
+
+        for (char c : input.toCharArray()) {
+            if (Character.isLetter(c)) {
+                alphabetSet.add(c);
+            }
+        }
+
+        return alphabetSet.size() == 26;
+    }
+}
